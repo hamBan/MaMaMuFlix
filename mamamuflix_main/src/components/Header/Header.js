6f6370ba93c0bdf4a9865a41a9c1dd990667cc5c
Subhankar200015 <roysubhankar123456789@gmail.com>
2025-05-24 12:30:13 +0530

    Side Drawer Done

--- mamamuflix_main/src/components/Header/Header.js
+++ mamamuflix_main/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './SiteHeader.css';
+import Sidebar from './Sidebar'
 
 function SiteHeader() {
   return (
@@ -10,15 +11,7 @@ function SiteHeader() {
         <Link to="/home" className="nav-item" aria-current="page">Home</Link>
         <Link to="/movies" className="nav-item">Movies</Link>
         <Link to="/series" className="nav-item">Series</Link>
-
-        <input type="checkbox" id="menu-toggle" />
-        <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
-        <nav className="navbar">
-          <ul className="nav-links">
-            <li><Link to="/">My List</Link></li>
-            <li><Link to="/">Contact</Link></li>
-          </ul>
-        </nav>
+        <Sidebar />
       </div>
     </header>
   );
