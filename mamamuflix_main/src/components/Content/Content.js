a8471fbdd2f0163bebe0721932326fd445031293
hamBan <bsoham103@gmail.com>
2025-05-24 14:22:19 +0530

    Fixed messed up merge conflicts.

--- mamamuflix_main/src/components/Content/Content.js
+++ mamamuflix_main/src/components/Content/Content.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Content.css';
 import FeaturedCard from './FeaturedCard/FeaturedCard';
-import Poster1 from '../../assets/parasite.jpg';
-import Poster2 from '../../assets/pather_panchali.jpg';
-import Poster3 from '../../assets/the_shawshank_redemption.png';
-
+import allItems from '../../data/all.json';
 
 
 function Content() {
+  const latestItems = allItems.slice(0, 4); // Connection to API endpoint needed
   return (
     <main className="content">
       <section className="featured">
         <h2>New Arrivals</h2>
         <div className="grid">
-          <FeaturedCard imageUrl={Poster1} altText="Parasite" title="Parasite" />
-          <FeaturedCard imageUrl={Poster2} altText="Pather Panchali" title="Pather Panchali" />
-          <FeaturedCard imageUrl={Poster3} altText="The Shawshank Redemption" title="The Shawshank Redemption" />
+          {latestItems.map(item => (
+            <FeaturedCard
+              imageUrl={item.imageUrl}
+              altText={item.altText}
+              title={item.title}
+            />
+          ))}
         </div>
       </section>
     </main>
--- mamamuflix_main/src/components/Grid/Grid.js
+++ mamamuflix_main/src/components/Grid/Grid.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Grid.css';
 import GridBox from './GridBox/GridBox';
-import Poster1 from '../../assets/parasite.jpg';
-import Poster2 from '../../assets/pather_panchali.jpg';
-import Poster3 from '../../assets/the_shawshank_redemption.png';
+import allItems from '../../data/all.json'; // API endpoint needed
 
-
+const itemsPerPage = 12
 
 function Grid() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = allItems.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(allItems.length / itemsPerPage);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <main className="content">
       <section className="featured">
-        <h2>New Arrivals</h2>
+        <h2>Currently Available</h2>
         <div className="grid">
-          <GridBox imageUrl={Poster1} altText="Parasite" title="Parasite" />
-          <GridBox imageUrl={Poster2} altText="Pather Panchali" title="Pather Panchali" />
-          <GridBox imageUrl={Poster3} altText="The Shawshank Redemption" title="The Shawshank Redemption" />
+          {currentItems.map(item => (
+            <GridBox
+              // key={item.id}
+              imageUrl={item.imageUrl}
+              altText={item.altText}
+              title={item.title}
+            />
+          ))}
         </div>
+
+        {totalPages > 1 && (
+          <div className="pagination">
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+              (pageNumber) => (
+                <button
+                  key={pageNumber}
+                  onClick={() => handlePageChange(pageNumber)}
+                  className={currentPage === pageNumber ? 'active' : ''}
+                >
+                  {pageNumber}
+                </button>
+              )
+            )}
+          </div>
+        )}
       </section>
     </main>
   );
