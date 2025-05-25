797ccdd3205e501288fd6453c9f309f364a9f6e7
Subhankar200015 <roysubhankar123456789@gmail.com>
2025-05-25 12:13:04 +0530

    search bar continuation

--- mamamuflix_main/src/components/Header/Header.js
+++ mamamuflix_main/src/components/Header/Header.js
@@ -7,14 +7,18 @@ import SearchBar from './SearchBar';
 function SiteHeader() {
   return (
     <header className="site-header">
-      <div className="container">
-        <Link to="/" className="logo">মামামুFLIX</Link>
-        <Link to="/home" className="nav-item" aria-current="page">Home</Link>
-        <Link to="/movies" className="nav-item">Movies</Link>
-        <Link to="/series" className="nav-item">Series</Link>
-        <SearchBar />
-        <Sidebar />
-      </div>
+      <div className="container1">
+        <div className='container_left'>
+          <Link to="/" className="logo">মামামুFLIX</Link>
+          <Link to="/home" className="nav-item" aria-current="page">Home</Link>
+          <Link to="/movies" className="nav-item">Movies</Link>
+          <Link to="/series" className="nav-item">Series</Link>
+        </div>
+        <div className='container_right'>
+          <SearchBar />
+          <Sidebar />
+        </div>
+        </div>
     </header>
   );
 }
