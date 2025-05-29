cdd6f3851fc1cb54e29c10c242eb0fcb91efa62f
Subhankar200015 <roysubhankar123456789@gmail.com>
2025-05-29 23:07:11 +0530

    connecting backend and frontend for login and register buttons

--- mamamuflix_main/src/components/Auth/Login.js
+++ mamamuflix_main/src/components/Auth/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './Login.css';
 
 function Login() {
@@ -7,17 +8,36 @@ function Login() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const navigate = useNavigate();
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     // In a real app, you would send this data to your backend API
-    console.log('Login Data:', { identifier, password });
-    alert('Login attempted (check console)');
+    // console.log('Login Data:', { identifier, password });
+    // alert('Login attempted (check console)');
+    try {
+      const response = await axios.post('http://localhost:8080/api/checkUser', {
+        username: identifier,
+        password: password
+      });
+
+      if(response.data === "Success")
+        navigate('/home');
+
+      else {
+      setError("Invalid username/email or password.");
+    }
 
     // Reset form
     setIdentifier('');
     setPassword('');
     setError('');
+    }
+
+    catch (error){
+      setError("Login failed. Please try again");
+    }
   };
 
   return (
